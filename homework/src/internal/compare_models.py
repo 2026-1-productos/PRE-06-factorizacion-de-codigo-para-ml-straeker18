24cc16675cf5ea51860eae6531b8a9f39b2effdc
Santiago Arango Villa <49077085+straeker18@users.noreply.github.com>
2026-05-23 12:17:18 -0500

    first_class

---
+++ homework/src/internal/compare_models.py
@@ -0,0 +1,10 @@
+# compare_models.py
+
+
+def compare_models(current_model, best_model, x_test, y_test):
+    """Compare the current model with the best model and return the better one."""
+    if best_model is None or current_model.score(x_test, y_test) > best_model.score(
+        x_test, y_test
+    ):
+        return current_model
+    return best_model
--- homework/src/train_elasticnet.py
+++ homework/src/train_elasticnet.py
@@ -14,22 +14,29 @@ from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
+
 # descarga de datos
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-df = pd.read_csv(url, sep=";")
+def prepare_data():
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    df = pd.read_csv(url, sep=";")
 
-# preparacion de datos
-y = df["quality"]
-x = df.copy()
-x.pop("quality")
+    # preparacion de datos
+    y = df["quality"]
+    x = df.copy()
+    x.pop("quality")
 
-# dividir los datos en entrenamiento y testing
-(x_train, x_test, y_train, y_test) = train_test_split(
-    x,
-    y,
-    test_size=0.25,
-    random_state=123456,
-)
+    # dividir los datos en entrenamiento y testing
+    x_train, x_test, y_train, y_test = train_test_split(
+        x,
+        y,
+        test_size=0.25,
+        random_state=123456,
+    )
+
+    return x_train, x_test, y_train, y_test
+
+
+x_train, x_test, y_train, y_test = prepare_data()
 
 # entrenar el modelo
 estimator = ElasticNet(alpha=0.5, l1_ratio=0.5, random_state=12345)
